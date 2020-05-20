cda2cee017167261a8ebe7f4a8b85cca1e833f84
Julien Collaer <collaer@gmail.com>
2020-05-20 19:51:22 -0400

    initial project

---
+++ dico.h
@@ -0,0 +1,9 @@
+/*dico.h
+------
+Contient les prototypes des fonctions de dico.c
+*/
+#ifndef DEF_DICO
+#define DEF_DICO
+int piocherMot(char *motPioche);
+int nombreAleatoire(int nombreMax);
+#endif
